38aed4c3a41da47df97f56fba009a16badd0505e
Likhithabz <161535522+Likhithabz@users.noreply.github.com>
2025-05-03 22:43:07 -0700

    Initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,153 @@
+provider "aws" {
+  region = var.region
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+  token = var.aws_session_token
+}
+
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.1"
+
+  name                 = "eks-blue-green"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    "kubernetes.io/cluster/${var.project_name}" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.project_name}" = "shared"
+    "kubernetes.io/role/elb"                    = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.project_name}" = "shared"
+    "kubernetes.io/role/internal-elb"           = "1"
+  }
+}
+
+resource "aws_db_subnet_group" "this" {
+  name       = "rds-subnet-group"
+  subnet_ids = module.vpc.private_subnets
+
+  tags = {
+    Name = "rds-subnet-group"
+  }
+}
+
+resource "aws_security_group" "rds" {
+  name        = "rds-sg"
+  description = "Allow access to RDS"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = [module.vpc.vpc_cidr_block] # Allow from internal VPC
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "rds-sg"
+  }
+}
+
+resource "aws_db_instance" "postgres" {
+  identifier             = "app-postgres-db"
+  engine                 = "postgres"
+  engine_version         = "15.12"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 20
+  db_subnet_group_name   = aws_db_subnet_group.this.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  username               = var.db_username
+  password               = var.db_password
+  publicly_accessible    = false
+  skip_final_snapshot    = true
+  db_name                = var.db_name
+}
+
+/*output "rds_endpoint" {
+  value = "${aws_db_instance.postgres.endpoint}"
+}*/
+
+//eks cluster
+resource "aws_eks_cluster" "eks_terraform" {
+  name     = var.project_name
+  version = "1.31"
+  //create_iam_role = false
+  role_arn = "arn:aws:iam::343830488876:role/LabRole"
+  vpc_config {
+    subnet_ids = module.vpc.private_subnets
+  }
+}
+
+resource "aws_eks_node_group" "my_node_group" {
+  cluster_name    = aws_eks_cluster.eks_terraform.name
+  node_group_name = "my-node-group"
+  node_role_arn   = "arn:aws:iam::343830488876:role/LabRole"
+  subnet_ids = module.vpc.private_subnets
+  scaling_config {
+    desired_size = 2
+    max_size     = 4
+    min_size     = 2
+  }
+  disk_size = 20
+  instance_types = ["t3.medium"]
+  capacity_type = "ON_DEMAND"
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = aws_eks_cluster.eks_terraform.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = aws_eks_cluster.eks_terraform.cluster_id
+}
+
+
+
+/*
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = var.project_name
+  cluster_version = "1.21"
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  node_groups = {
+    default = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 2
+      create_iam_role  = false
+      instance_profile = "arn:aws:iam::343830488876:instance-profile/eks-cacb49a9-e47a-61c9-6917-259705511f1c"
+      disk_size        = 20
+      instance_types = ["t3.medium"]
+      capacity_type    = "ON_DEMAND"
+
+      tags = {
+        Name = "eks-node-group-default"
+      }
+    }
+  }
+}
+*/
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,47 @@
-variable "instance_type$END$" {
-  type        = string                     # The type of the variable, in this case a string
-  default     = "t2.micro"                 # Default value for the variable
-  description = "The type of EC2 instance" # Description of what this variable represents
+variable "db_name" {
+  default = "mydb"
+}
+
+variable "aws_access_key_id" {
+  default = "ASIAVADPE44WP63PRN7H"
+}
+
+variable "aws_secret_access_key" {
+  default = "PkA+9KU3hzmbDBZsMucUpTcsF4ksokYcbyfn+400"
+}
+
+variable "aws_session_token" {
+  default = "IQoJb3JpZ2luX2VjEFEaCXVzLXdlc3QtMiJGMEQCIF91lYQ9D/dMFxNnzsNaGGGqVFS4UFywF7z4wVWtRnM0AiATfOdIZTmCtzDBHY6I6ZW8w/ZzSH3QXsV6iXvOaJue2Sq0Agjq//////////8BEAEaDDM0MzgzMDQ4ODg3NiIM1HINRF9sP5yC64PqKogCUddhysknJ92ErulUP+n6J2qIBmvdxYxIei1u0YCRgOWyWp4gCTARvxcGY5Dwi07LdgKg3eCisdlMStyaB8OPKugjGyfyqbQRXzB70HqniXoEYFb1e8w+cIMxekq3EpJdMjYQAH7O/g27cAQFPXCstSw/NMrQC86T3ABj4efUYywX+WULsTQOrLhgVEqY2/18z/zHn7QSMdAoHaD+UjXjkD5u2yW4LxpTGT68esrmbV6y1wBROQWaEHcG/ixsK0rT+BffejBEe9JlMquxy6jycCptiHV771CovDYwo/k/yFKSJlOyUxGopJQcKkUq13xNJgxE8+ggqvJXeaWDw7DgmPLY+xAQIowZMNC518AGOp4BLCd5HZFG5qd0MAWYkKsbKaTxKjru59K/qZjgfZQvUvymDvtAnSXXtaVprm520nyLSJVmfeYsdUg3IktTIorA8PBLJH1SQjiQ+jnHRUU8DZj+g+h5LQubp2h5TGik57N2AVjNL7L28JPPcl4jNHR8F6PG6C3W0nNh6/IFKmj5TtTTmgn/D9YhqRglPmTcOuO8/h0VcgCPxPgdjekFAfs="
+}
+
+variable "db_username" {
+  default = "postgres"
+}
+
+variable "db_password" {
+  default = "likhitha"
+}
+
+variable "db_instance_class" {
+  default = "db.t3.micro"
+}
+
+variable "allocated_storage" {
+  default = 20
+}
+
+variable "engine_version" {
+  default = "15.3"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "profile" {
+  default = "default"
+}
+
+variable "project_name" {
+  default = "eks-blue-green"
 }
